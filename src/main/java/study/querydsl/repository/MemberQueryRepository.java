deda729d65298a7577b281197e134e4c266a7806
hyechan <dhgpcks@gmail.com>
2022-03-31 23:15:46 +0900

    paging simple 처리

---
+++ src/main/java/study/querydsl/repository/MemberQueryRepository.java
@@ -0,0 +1,77 @@
+package study.querydsl.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.springframework.util.StringUtils.hasText;
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+/**
+ * fileName    : MemberQueryRepository
+ * author      : hyechan
+ * date        : 2022/03/31
+ * description :
+ * 조회용 Repository
+ * MemberRepositoryCustom, 특화된 기능. 분리해서 개발할 수 있다.
+ * 모든 걸 Custom에 때려 넣을 필요가 없다.
+ * 공용성 없고, 특정 API에 종속적이면.. 그냥 따로 쓰기도 한다.
+ * 찾기도 편함.
+ * ====================================================
+ * DATE              AUTHOR               NOTE
+ * ----------------------------------------------------
+ * 2022/03/31 9:07 오후  hyechan        최초 생성
+ */
+@Repository
+public class MemberQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public MemberQueryRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition){
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username, member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName"))
+                )
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null ;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
+
+}
---
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -0,0 +1,24 @@
+package study.querydsl.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+
+import java.util.List;
+
+/**
+ * fileName    : MemberRepositoryCustom
+ * author      : hyechan
+ * date        : 2022/03/31
+ * description :
+ * ====================================================
+ * DATE              AUTHOR               NOTE
+ * ----------------------------------------------------
+ * 2022/03/31 8:59 오후  hyechan        최초 생성
+ */
+public interface MemberRepositoryCustom {
+    List<MemberTeamDto> search(MemberSearchCondition condition);
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
+}
